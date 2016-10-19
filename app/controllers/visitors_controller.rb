9e6f2a66e5c1bfe501c92f0e3d77e3b9b4663e79
Jordan Nguyen <jordannguyen95@gmail.com>
2016-10-19 12:06:56 -0700

    update application layout with rails_layout gem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,9 @@ class VisitorsController < ApplicationController
 	def new
 		Rails.logger.debug('DEBUG: Entering new method')
 		@owner = Owner.new
-		
+		#hidden render 'visitors/new' method being called here
+		#render combines application.html.erb with new.html.erb
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birhtday is soon.'
 	end
 end
